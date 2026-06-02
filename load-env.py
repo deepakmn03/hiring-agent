58d4de2a07050bfff91b88a9b8cbdfe645b1487b
deepakmn03 <meenadeepu22@gmail.com>
2026-06-02 13:28:26 +0530

    Feat: Update environment configuration for Gemini provider and add load-env script

---
+++ load-env.py
@@ -0,0 +1,5 @@
+import os
+from dotenv import load_dotenv
+
+# Automatically loads variables from .env into os.environ
+load_dotenv() 
---
+++ set-env.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+export LLM_PROVIDER=ollama
+export DEFAULT_MODEL=llama3.2:latest
